3605ae3ce9ddd113adf8f3ddf116b32e0c96ce54
Awsafaneh <awsafaneh9@gmail.com>
2025-05-16 17:38:27 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//Config Database
+define('DB_HOST', 'localhost');
+define('DB_USER', '**********');
+define('DB_PASS', '**********');
+define('DB_NAME', '**********');
+define('TIMEZONE', 'Asia/Kolkata');
+define('ENCRYPTION_KEY', 'c79fc2b9c284a26f5b240dc3b836139e');
